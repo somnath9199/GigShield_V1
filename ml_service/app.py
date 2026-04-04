654553027e40741ea78bb416ebc582476485a6d1
Somnath dey <106850450+somnath9199@users.noreply.github.com>
2026-04-04 15:08:07 +0530

    Update app.py

--- ml_service/app.py
+++ ml_service/app.py
@@ -184,7 +184,6 @@ def predict_curfew():
         })
     except Exception as e:
         return jsonify({"success": False, "message": str(e)}), 500
+port = int(os.environ.get("PORT", 10000))
+app.run(host='0.0.0.0', port=port)
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
