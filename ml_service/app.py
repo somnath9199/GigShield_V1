671b540739a9fc5d1899f401f6794446142b0453
Somnath dey <106850450+somnath9199@users.noreply.github.com>
2026-04-04 16:46:21 +0530

    Update app.py

--- ml_service/app.py
+++ ml_service/app.py
@@ -14,6 +14,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+    return "Server is live"
+
+
 # ==========================================
 # 1. LOAD DYNAMIC PRICING MODEL (XGBoost)
 # ==========================================
