73ac1e7e85b99a0d0eb98a6734d385e121ec05d0
Somnath dey <106850450+somnath9199@users.noreply.github.com>
2026-04-04 22:50:44 +0530

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const supabase = require("./server/config/supabase");
-async function test() {
-  const { data, error } = await supabase.from("disruptions").select("*");
-  console.log("data:", JSON.stringify(data, null, 2));
-  console.log("error:", error);
-}
-test();
